b1c2723a44f7eec89a57196a1d694974558e1b3e
DESKTOP-G5709PJ\SuperguyX <superguy.273p@gmail.com>
2021-01-10 14:35:03 +0700

    add name

---
+++ variable/name.py
@@ -0,0 +1,13 @@
+#สามารถประกาศได้
+myvar = "Guy"
+my_var = "Guy"
+_myvar = "Guy"
+myVar = "Guy"
+myvar2 = "Guy"
+
+#ไม่สามารถประกาศได้
+2myvar = "Guy"
+my-var ="guy"
+my var = 'Guy'
+
+#ตัวแปลตัวใหญ่หมดเท่ากับค่าคงที่
