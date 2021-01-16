ee715a60ff3ceb3bc40445ccddc8eaeff50d67a9
DESKTOP-G5709PJ\SuperguyX <superguy.273p@gmail.com>
2021-01-16 15:36:33 +0700

    string

---
+++ string/modify_str.py
@@ -0,0 +1,7 @@
+string = " Hello, World! "
+print(string.upper())  # "HELLO, WORLD!"
+print(string.lower())  # hello, world!
+print(string.strip())  # Hello, World!
+print(string.replace("H", "J"))  # Jello, World!
+print(string.split())[" Hello", "World!"]
+print(len(string))  # 15
---
+++ string/test.py
@@ -0,0 +1,18 @@
+string = "I am the best programmer"
+print(len(string))
+
+print(string[0])
+
+print(string[9:13])
+
+print(string.replace(" ", ""))
+
+print(string.upper())
+
+print(string.lower())
+
+print(string.replace("e", "z"))
+
+myname = "Guy"
+txt = "{} is the best programmer"
+print(txt.format(myname))
